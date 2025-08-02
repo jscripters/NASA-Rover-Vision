d502fc26d919c5dfc90fe463d05ae279fab3ef87
Gabriel Walker <gabrielwalker@Gabriels-MacBook-Air.local>
2025-08-02 10:22:04 -0400

    added sockets

---
+++ project/app/socket/socket.js
@@ -0,0 +1,70 @@
+let createServer = require('node:http');
+let  Server = require('socket.io');
+const sqlite3 = require('sqlite3');
+const open = require('sqlite');
+
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {}
+});
+
+async function setupSocket() {
+  // open the database file
+  const db = await open({
+      filename: 'chat.db',
+      driver: sqlite3.Database
+  });
+
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
+
+  io.on('connection', async (socket) => {
+    console.log(`${socket.id} connected`);
+
+    if (!socket.recovered) {
+      try {
+        const timeCutoff = new Date(Date.now() - 30 * 60 * 1000); // 30 minutes ago
+        const recentMessages = await db.all(
+          'SELECT * FROM messages WHERE timestamp >= ? AND id > ? ORDER BY timestamp ASC',
+          [timeCutoff.toISOString(), socket.handshake.auth.serverOffset || 0]
+        );
+
+        recentMessages.forEach(row => {
+          socket.emit('chat message', row.content, row.id);
+        });
+
+      } catch (e) {
+        // something went wrong
+        console.error(e);
+      }
+    }
+
+    socket.on('chat message', async (msg, client_offset, timeStamp, callback) => {
+      let result;
+      try {
+        result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?)', msg, client_offset, timeStamp);
+      } catch (e) {
+        if (e.errno === 19) {
+          callback({ success: false, error: err.message });
+        } else {
+          callback({ success: false, error: 'An error occurred' });
+        }
+        return;
+      }
+      io.emit('chat message', msg, result.lastID);
+      callback({ success: true });
+    });
+
+    socket.on('disconnect', () => {
+      console.log(`${socket.id}: disconnected`);
+    });
+  });
+}
+
+module.exports = { setupSocket };

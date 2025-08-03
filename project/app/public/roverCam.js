41d8fa6dad70e3fc8eceb50f6d397ebac97767d8
KakitZhu-17 <zhukakit17@gmail.com>
2025-08-02 23:49:12 -0400

    filters api for days with the most pictures for reccomendation

--- project/app/public/roverCam.js
+++ project/app/public/roverCam.js
@@ -5,12 +5,47 @@ let prevButton = document.getElementById("previous");
 let timelapseButton = document.getElementById("timelapse");
 let photosArr = []
 
+let recContainer = document.getElementById("add");
+function setRecTable(array){
+    for(let i = 0;i < array.length;i++){
+        let addSolDay = document.createElement("p");
+        addSolDay.textContent = array[i]
+        console.log(array[i])
+        recContainer.appendChild(addSolDay)
+    }
+}
+
+
+function getManifest(recArr,rover){
+    let url = `/getManifest?rover=${rover}`;
+    fetch(url).then((response) => {
+        return response.json();
+    })
+    .then((body) => {
+        //console.log("body:",body.length);
+        let manifestLen = body.length
+        for(let i = 0; i< manifestLen;i++){
+            if(body[i].total_photos >= 300){
+                recArr.push(body[i].sol)
+            }
+        }
+        console.log(recArr)
+        setRecTable(recArr)
+    }).catch(error=> {
+        console.log(error);
+    }
+    );
+}
+
+
 function submit(){
     let dayInput = document.getElementById("day").value;
     let roverInput = document.getElementById("rover").value;
+    let cameraInput = document.getElementById("camera").value;
     console.log(dayInput);
     console.log(roverInput);
-    let url = `/getPhotos?solday=${dayInput}&rover=${roverInput}`;
+    console.log(cameraInput);
+    let url = `/getPhotos?solday=${dayInput}&camera=${cameraInput}&rover=${roverInput}`;
     fetch(url).then((response) => {
         return response.json();
     })
@@ -48,8 +83,19 @@ function getNextPhotos(){
 function timelaspe(){
     setInterval(function () {
     getNextPhotos()
-  }, 700);
+  }, 600);
 }
+
+const roverInput =document.getElementById("rover");
+let roverName =document.getElementById("rovername");
+roverInput.addEventListener('change', (event) => {
+    let reccomendations = []
+    recContainer.textContent="";
+    console.log(roverInput.value);
+    roverName.textContent = roverInput.value;
+    getManifest(reccomendations,roverInput.value)}
+);
+
 
 button.addEventListener("click",submit);
 console.log(photosArr)
--- project/app/server.js
+++ project/app/server.js
@@ -13,10 +13,25 @@ app.get("/", (req, res) => {
   res.sendFile("public/login.html", { root: __dirname });
 });
 
+
+app.get("/getManifest", (req, res) => {
+  let rover = req.query.rover;
+  let url = `${baseUrl}manifests/${rover}?api_key=${apiKey}`;
+  axios.get(url).then((response) => {
+    //console.log("Received response manifest:", response.data.photo_manifest.photos);
+    res.json(response.data.photo_manifest.photos);
+  }).catch(error => {
+    console.log(error.message);
+    let errorCode = parseInt(error.code);
+    res.status(errorCode).json({"error":error.message});
+  });
+  console.log(`Sending request to: ${url}`);
+});
+
 app.get("/getPhotos", (req, res) => {
   let rover = req.query.rover;
   let solDay = req.query.solday;
-  const camera = req.query.camera || "navcam";
+  let camera = req.query.camera || "navcam";
   
   //console.log(rover,solDay,apiKey)
   let url = `${baseUrl}rovers/${rover}/photos?sol=${solDay}&camera=${camera}&api_key=${apiKey}`;

addd389ed466ae433198490512b07e0b8f5ffb6b
KakitZhu-17 <zhukakit17@gmail.com>
2025-08-08 00:55:32 -0400

    fixed bug regarding timelapse not reseting after changing sol days and adds pause and previous button functionalities

--- project/app/public/roverCam.js
+++ project/app/public/roverCam.js
@@ -3,14 +3,14 @@ let images = document.getElementById("photos");
 let nextButton = document.getElementById("next");
 let prevButton = document.getElementById("previous");
 let timelapseButton = document.getElementById("timelapse");
-let photosArr = []
+let pauseButton = document.getElementById("pause");
 
 let recContainer = document.getElementById("add");
 function setRecTable(array){
     for(let i = 0;i < array.length;i++){
         let addSolDay = document.createElement("p");
         addSolDay.textContent = array[i]
-        console.log(array[i])
+        //console.log(array[i])
         recContainer.appendChild(addSolDay)
     }
 }
@@ -37,8 +37,46 @@ function getManifest(recArr,rover){
     );
 }
 
+let currentSrc = 0;
+let isIntervalOn = false;
+let interval;
 
-function submit(){
+function timelaspe(){
+    interval = setInterval(function () {
+    getNextPhotos()
+  }, 600);
+  isIntervalOn = true
+}
+
+function stopInterval(){
+    clearInterval(interval)
+    isIntervalOn = false;
+}
+
+
+
+function getNextPhotos(){
+    if(currentSrc < photosArr.length){
+        images.src =photosArr[currentSrc];
+        currentSrc+=1;
+    }
+    else{
+        currentSrc= 0;
+    }
+}
+
+function getPrevPhotos(){
+    if(currentSrc > 0){
+        images.src =photosArr[currentSrc];
+        currentSrc-=1;
+    }
+    else{
+        currentSrc= 0;
+    }
+}
+
+function submit(srcArr){
+    stopInterval();
     let dayInput = document.getElementById("day").value;
     let roverInput = document.getElementById("rover").value;
     let cameraInput = document.getElementById("camera").value;
@@ -55,10 +93,10 @@ function submit(){
 
         for(let i = 0; i< maxLen;i++){
             imageSource = body.photos[i].img_src.toString()
-            photosArr.push(imageSource)
+            srcArr.push(imageSource)
         }
         
-        if(photosArr.length > 0){
+        if(srcArr.length > 0){
             images.src = body.photos[0].img_src.toString()
         }
 
@@ -68,22 +106,19 @@ function submit(){
     );
 }
 
-let i = 0
-function getNextPhotos(){
-    //console.log(i)
-    if(i < photosArr.length){
-        images.src =photosArr[i];
-        i+=1;
+function nextButtonClicked(){
+    if(isIntervalOn){
+        stopInterval()
     }
-    else{
-        i = 0;
-    }
+    getNextPhotos()
 }
 
-function timelaspe(){
-    setInterval(function () {
-    getNextPhotos()
-  }, 600);
+function prevButtonClicked(){
+    if(isIntervalOn){
+        stopInterval()
+    }
+    getPrevPhotos()
+    
 }
 
 const roverInput =document.getElementById("rover");
@@ -91,13 +126,24 @@ let roverName =document.getElementById("rovername");
 roverInput.addEventListener('change', (event) => {
     let reccomendations = []
     recContainer.textContent="";
-    console.log(roverInput.value);
+    //console.log(roverInput.value);
     roverName.textContent = roverInput.value;
     getManifest(reccomendations,roverInput.value)}
 );
 
+let photosArr = []
+let firstClick = true;
+function submitClick(){
+    if(!firstClick){
+        photosArr=[]
+        currentSrc=0;
+    }
+    submit(photosArr)
+    firstClick=false
+}
 
-button.addEventListener("click",submit);
-console.log(photosArr)
-nextButton.addEventListener("click",getNextPhotos);
+button.addEventListener("click",submitClick);
+nextButton.addEventListener("click",nextButtonClicked);
+prevButton.addEventListener("click",prevButtonClicked);
+pauseButton.addEventListener("click",stopInterval);
 timelapseButton.addEventListener("click",timelaspe);
